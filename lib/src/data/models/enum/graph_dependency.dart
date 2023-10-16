c804e02ae83aca668a60558c9450728182d8ca06
dexorded <126596523+dexorded@users.noreply.github.com>
2023-10-16 20:34:55 +0300

    feat: create Enum for models

---
+++ lib/src/data/models/enum/graph_dependency.dart
@@ -0,0 +1,10 @@
+/// [average] - среднее, [minimal] - минимальное, [maximum] - максимальное, [addiction] - линейная зависимость
+/// [scattePlot] - диаграмма рассеивания, [sensors] - дитчики
+enum GraphDependency {
+  average,
+  minimal,
+  maximum,
+  addiction,
+  scatterPlot,
+  sensors,
+}
---
+++ lib/src/data/models/enum/graph_type.dart
@@ -0,0 +1,6 @@
+/// [linear] - линейный график, [columnar] - столбчатая диаграмма, [spot] - точечный график
+enum GraphType {
+  linear,
+  columnar,
+  spot,
+}
