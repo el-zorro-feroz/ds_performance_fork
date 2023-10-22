ac1adcf984ffcd0edd43446d8237ab8791e1e332
Daniil <115182119+danwirele@users.noreply.github.com>
2023-10-22 15:49:37 +0300

    refactor: edit grama

--- lib/src/data/models/enum/graph_dependency.dart
+++ lib/src/data/models/enum/graph_dependency.dart
@@ -1,5 +1,5 @@
 /// [average] - среднее, [minimal] - минимальное, [maximum] - максимальное, [addiction] - линейная зависимость
-/// [scattePlot] - диаграмма рассеивания, [sensors] - датчики
+/// [scatterPlot] - диаграмма рассеивания, [sensors] - датчики
 enum GraphDependency {
   average,
   minimal,
