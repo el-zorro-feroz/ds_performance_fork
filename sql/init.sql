eb9f236ae0e5a83ad2a5ff31d76a0dc74d227f87
danwirele <115182119+danwirele@users.noreply.github.com>
2023-10-23 16:33:25 +0300

    feat: danya commit

--- sql/init.sql
+++ sql/init.sql
@@ -1,30 +1,30 @@
 CREATE TABLE IF NOT EXISTS Configs (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     title VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Tabs (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     config_id UUID,
     FOREIGN KEY(config_id) REFERENCES Configs (id),
     title VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Sensors (
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     config_id UUID,
     FOREIGN KEY(config_id) REFERENCES Configs (id),
     title VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Graphs (
-    id UUID PRIMARY KEY, 
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY, 
     type GraphType NOT NULL,
     dependency GraphDependency NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS GraphSensors(
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     graphs_id UUID,
     sensor_id UUID,
     FOREIGN KEY(graphs_id) REFERENCES Graphs (id),
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS GraphSensors(
 );
 
 CREATE TABLE IF NOT EXISTS TabSensors(
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     sensor_id UUID,
     tab_id UUID,
     FOREIGN KEY(sensor_id) REFERENCES Sensors (id),
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS TabSensors(
 );
 
 CREATE TABLE IF NOT EXISTS Rules(
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     description VARCHAR UNIQUE NOT NULL
 );
 
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS SensorRules(
 );
 
 CREATE TABLE IF NOT EXISTS SensorsHistory(
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     sensor_id UUID,
     FOREIGN KEY(sensor_id) REFERENCES Sensors (id),
     date timestamp NOT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE IF NOT EXISTS SensorsHistory(
 );
 
 CREATE TABLE IF NOT EXISTS Alerts(
-    id UUID PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     sensor_id UUID,
     rule_id UUID,
     FOREIGN KEY(sensor_id) REFERENCES Sensors (id),
