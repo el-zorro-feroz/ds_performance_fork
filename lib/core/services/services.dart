d9c67845cd38a59d71ef7c70771be5a1bc45ae8d
diffygg <np.01@inbox.ru>
2023-10-29 18:45:49 +0300

    feat: create PostgresModule

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -1,9 +1,22 @@
+import 'dart:async';
+
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:postgres/postgres.dart';
 
 import 'services.config.dart';
 
 final GetIt services = GetIt.I;
 
 @InjectableInit()
-void servicesInit() => services.init();
+Future<void> servicesInit() async => services.init();
+
+@Injectable()
+class PostgresModule {
+  static late PostgreSQLConnection postgreSQLConnection;
+
+  FutureOr<void> initPostgres() async {
+    postgreSQLConnection = PostgreSQLConnection("127.0.0.1", 5432, "postgres", username: "postgres", password: "1");
+    await postgreSQLConnection.open();
+  }
+}
