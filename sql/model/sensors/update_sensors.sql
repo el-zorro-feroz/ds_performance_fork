b9ab71a22bc5f9940d3f503ba1fb2de7479e1851
diffygg <np.01@inbox.ru>
2023-10-31 16:04:31 +0300

    feat: add parameter to queries

--- sql/model/sensors/update_sensors.sql
+++ sql/model/sensors/update_sensors.sql
@@ -1,6 +1,7 @@
 UPDATE sensors
 -- title VARCHAR
 SET title = COALESCE (@title, title),
-    type = COALESCE (@type, type)
+    type = COALESCE (@type, type),
+	details = COALESCE (@details, details)
 -- id uuid
 WHERE id = @id;
