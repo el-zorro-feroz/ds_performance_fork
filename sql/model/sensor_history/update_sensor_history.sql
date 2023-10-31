033fa43cfe9e8e5ea37c890dc4229d1b32ea57b1
diffygg <np.01@inbox.ru>
2023-10-31 17:49:28 +0300

    fix: change sql query

--- sql/model/sensor_history/update_sensor_history.sql
+++ sql/model/sensor_history/update_sensor_history.sql
@@ -2,8 +2,8 @@ UPDATE sensorhistory
 -- sensor_id uuid
 -- date timestamp
 -- value real
-SET sensor_id = @sensor_id,
-	date = @date,
-	value = @value
+SET sensor_id = COALESCE (@sensor_id, sensor_id),
+	date = COALESCE (@date, date),
+	value = COALESCE (@value, value)
 -- id uuid
 WHERE id = @id;
