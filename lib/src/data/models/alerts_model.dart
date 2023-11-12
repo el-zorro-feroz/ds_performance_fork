b80e07abcf7c90a947a7eca48b048f25436fc525
dexorded <126596523+dexorded@users.noreply.github.com>
2023-11-12 19:58:49 +0300

    feat: add params AlertType for AlertsModel

--- lib/src/data/models/alerts_model.dart
+++ lib/src/data/models/alerts_model.dart
@@ -1,13 +1,17 @@
+import 'package:sensors_monitoring/src/data/models/enum/alert_type.dart';
+
 class AlertsModel {
   final String id;
   final String sensorId;
   final String ruleId;
+  final AlertType type;
   final String message;
 
   const AlertsModel({
     required this.id,
     required this.sensorId,
     required this.ruleId,
+    required this.type,
     required this.message,
   });
 
@@ -15,12 +19,14 @@ class AlertsModel {
     String? id,
     String? sensorId,
     String? ruleId,
+    AlertType? type,
     String? message,
   }) {
     return AlertsModel(
       id: id ?? this.id,
       sensorId: sensorId ?? this.sensorId,
       ruleId: ruleId ?? this.ruleId,
+      type: type ?? this.type,
       message: message ?? this.message,
     );
   }
@@ -28,8 +34,9 @@ class AlertsModel {
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
-      'sensorId': sensorId,
-      'ruleId': ruleId,
+      'sensor_id': sensorId,
+      'rule_id': ruleId,
+      'type': type.name,
       'message': message,
     };
   }
@@ -37,8 +44,9 @@ class AlertsModel {
   factory AlertsModel.fromMap(Map<String, dynamic> map) {
     return AlertsModel(
       id: map['id'] as String,
-      sensorId: map['sensorId'] as String,
-      ruleId: map['ruleId'] as String,
+      sensorId: map['sensor_id'] as String,
+      ruleId: map['rule_id'] as String,
+      type: AlertType.values.byName(map['type']),
       message: map['message'] as String,
     );
   }
