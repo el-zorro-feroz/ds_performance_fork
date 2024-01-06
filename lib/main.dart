abd4a40310cf1ce4585043fd0294c5284e149b29
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-01-06 13:31:44 +0000

    feat: launch notification fetching with application start

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:sensors_monitoring/core/services/services.dart';
+import 'package:sensors_monitoring/src/presentation/controllers/notification_controller.dart';
 import 'package:sensors_monitoring/src/presentation/service_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await servicesInit();
+  services<NotificationController>().fetchNotifications();
 
   runApp(const ServiceApp());
 }
