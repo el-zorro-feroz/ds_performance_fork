dbbe136b5feaee085a229964b00c4ac314adbd23
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-01-06 13:32:03 +0000

    feat: add notification time to entity

--- lib/src/domain/entities/notification_data.dart
+++ lib/src/domain/entities/notification_data.dart
@@ -1,13 +1,20 @@
-class NotificationData {
+import 'package:equatable/equatable.dart';
+
+class NotificationData extends Equatable {
   final NotificationType type;
   final String title;
   final String description;
+  final DateTime datetime;
 
   const NotificationData({
     required this.type,
     required this.title,
     required this.description,
+    required this.datetime,
   });
+
+  @override
+  List<Object?> get props => [datetime];
 }
 
 enum NotificationType { info, warning, error }
