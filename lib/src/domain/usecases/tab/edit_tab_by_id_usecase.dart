61b866bae334d4eafb72266952de30f3f01b0fc1
diffygg <np.01@inbox.ru>
2024-01-15 15:06:04 +0300

    feat: create usecase

---
+++ lib/src/domain/usecases/tab/edit_tab_by_id_usecase.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:sensors_monitoring/core/failure/failure.dart';
+import 'package:sensors_monitoring/core/usecase/usecase.dart';
+import 'package:sensors_monitoring/src/domain/entities/tab.dart';
+
+class EditTabByIdUsecase extends UseCase<Tab, EditTabUsecaseParams> {
+  @override
+  FutureOr<Either<Failure, Tab>> call(EditTabUsecaseParams param) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
+
+class EditTabUsecaseParams {}
