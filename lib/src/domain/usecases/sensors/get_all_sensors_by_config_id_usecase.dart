922fe449c2afffae5e62736cc86022734f334a26
diffygg <np.01@inbox.ru>
2024-01-17 18:21:39 +0300

    refactor: rename usecases

--- lib/src/domain/usecases/sensors/get_all_sensors_by_config_id_usecase.dart
+++ lib/src/domain/usecases/sensors/get_all_sensors_by_config_id_usecase.dart
@@ -5,7 +5,7 @@ import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
 import 'package:sensors_monitoring/src/domain/entities/sensor_info.dart';
 
-class GetAllConfigSensorUsecase extends UseCase<List<SensorInfo>, String> {
+class GetAllSensorsByConfigIdUsecase extends UseCase<List<SensorInfo>, String> {
   @override
   FutureOr<Either<Failure, List<SensorInfo>>> call(String param) {
     // TODO: implement call
--- lib/src/domain/usecases/tab/edit_tab_usecase.dart
+++ lib/src/domain/usecases/tab/edit_tab_usecase.dart
@@ -5,7 +5,7 @@ import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
 import 'package:sensors_monitoring/src/domain/entities/tab.dart';
 
-class EditTabByIdUsecase extends UseCase<Tab, EditTabUsecaseParams> {
+class EditTabUsecase extends UseCase<Tab, EditTabUsecaseParams> {
   @override
   FutureOr<Either<Failure, Tab>> call(EditTabUsecaseParams param) {
     // TODO: implement call
