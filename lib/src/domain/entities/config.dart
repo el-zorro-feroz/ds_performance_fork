c19a973a680da12eb693237c171118a851a10a2b
diffygg <np.01@inbox.ru>
2024-01-17 19:03:07 +0300

    fix

--- lib/src/domain/entities/config.dart
+++ lib/src/domain/entities/config.dart
@@ -1,14 +1,20 @@
 import 'package:equatable/equatable.dart';
+import 'package:sensors_monitoring/src/domain/entities/sensor_info.dart';
 import 'package:sensors_monitoring/src/domain/entities/tab.dart';
 
 class Config extends Equatable {
   final String id;
   final String title;
   final List<Tab> tabList;
+  // final List<TabSensor> tabSensorList;
+  final List<SensorInfo> sensorList;
+
   const Config({
     required this.id,
     required this.title,
     required this.tabList,
+    // required this.tabSensorList,
+    required this.sensorList,
   });
 
   @override
