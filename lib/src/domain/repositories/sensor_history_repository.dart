edec5e1f73abbe802f360fd663f8514d8143e392
diffygg <np.01@inbox.ru>
2024-01-17 19:07:14 +0300

    fix: remove unused repository

--- lib/src/domain/repositories/sensor_history_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:sensors_monitoring/core/failure/failure.dart';
-import 'package:sensors_monitoring/src/domain/entities/sensor_data.dart';
-import 'package:sensors_monitoring/src/domain/usecases/graphs/get_graph_history_by_sensor_id.dart';
-
-abstract class SensorHistoryRepository {
-  Future<Either<Failure, List<SensorHistory>>> getSensorHistory({required GetSensorHistoryParams param});
-}
