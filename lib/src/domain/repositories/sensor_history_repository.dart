c3eab0f5f40c2dd14a325a9fa10d3611f1024195
diffygg <np.01@inbox.ru>
2024-01-17 18:24:25 +0300

    fix: change return type

--- lib/src/domain/repositories/sensor_history_repository.dart
+++ lib/src/domain/repositories/sensor_history_repository.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
-import 'package:sensors_monitoring/src/domain/usecases/sensor_history/get_sensor_history.dart';
+import 'package:sensors_monitoring/src/domain/entities/sensor_data.dart';
+import 'package:sensors_monitoring/src/domain/usecases/graphs/get_graph_history_by_sensor_id.dart';
 
 abstract class SensorHistoryRepository {
-  Future<Either<Failure, List<SensorHistoryRepository>>> getSensorHistory({required GetSensorHistoryParams param});
+  Future<Either<Failure, List<SensorHistory>>> getSensorHistory({required GetSensorHistoryParams param});
 }
