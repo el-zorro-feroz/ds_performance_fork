5f6f08e903f27ea8948109409cd0dd1cd8bfda2a
diffygg <np.01@inbox.ru>
2024-01-22 17:31:20 +0300

    feat: add logic to usecases

--- lib/src/domain/usecases/configs/add_config_usecase.dart
+++ lib/src/domain/usecases/configs/add_config_usecase.dart
@@ -1,16 +1,22 @@
 import 'dart:async';
 
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
 import 'package:sensors_monitoring/src/domain/entities/config.dart';
 import 'package:sensors_monitoring/src/domain/entities/sensor_info.dart';
+import 'package:sensors_monitoring/src/domain/repositories/config_repository.dart';
 
+@Injectable()
 class AddConfigUsecase extends UseCase<Config, AddConfigUsecaseParams> {
+  final ConfigRepository configRepository;
+
+  AddConfigUsecase({required this.configRepository});
+
   @override
-  FutureOr<Either<Failure, Config>> call(AddConfigUsecaseParams param) {
-    // TODO: implement call
-    throw UnimplementedError();
+  FutureOr<Either<Failure, Config>> call(AddConfigUsecaseParams param) async {
+    return await configRepository.addConfig(param);
   }
 }
 
--- lib/src/domain/usecases/configs/get_all_configs_usecase.dart
+++ lib/src/domain/usecases/configs/get_all_configs_usecase.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
 
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
 import 'package:sensors_monitoring/src/domain/entities/config.dart';
+import 'package:sensors_monitoring/src/domain/repositories/config_repository.dart';
 
+@Injectable()
 class GetAllConfigsUsecase extends UseCase<List<Config>, Unit> {
+  final ConfigRepository configRepository;
+
+  GetAllConfigsUsecase({required this.configRepository});
+
   @override
-  FutureOr<Either<Failure, List<Config>>> call(Unit param) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, List<Config>>> call(Unit param) async {
+    return await configRepository.getAllConfigs();
   }
 }
