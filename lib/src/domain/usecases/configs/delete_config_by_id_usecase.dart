928adac1e631f8696667186a27533f403745ad87
diffygg <np.01@inbox.ru>
2024-01-23 16:32:15 +0300

    feat: add logic to usecase

--- lib/src/domain/usecases/configs/delete_config_by_id_usecase.dart
+++ lib/src/domain/usecases/configs/delete_config_by_id_usecase.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
 
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
+import 'package:sensors_monitoring/src/domain/repositories/config_repository.dart';
 
+@Injectable()
 class DeleteConfigByIdUsecase extends UseCase<Unit, String> {
+  final ConfigRepository configRepository;
+
+  DeleteConfigByIdUsecase({required this.configRepository});
+
   @override
-  FutureOr<Either<Failure, Unit>> call(String id) {
-    // TODO: implement call
-    throw UnimplementedError();
+  FutureOr<Either<Failure, Unit>> call(String id) async {
+    return await configRepository.deleteConfigById(id);
   }
 }
 
