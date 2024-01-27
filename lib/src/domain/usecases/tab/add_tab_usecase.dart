1dc4c5a25c62c8903ff721a60ff7c62ad47c6e2d
diffygg <np.01@inbox.ru>
2024-01-27 18:51:16 +0300

    feat: add logic to usecases

--- lib/src/domain/usecases/tab/add_tab_usecase.dart
+++ lib/src/domain/usecases/tab/add_tab_usecase.dart
@@ -1,24 +1,31 @@
 import 'dart:async';
 
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
+import 'package:sensors_monitoring/src/domain/entities/config.dart';
 import 'package:sensors_monitoring/src/domain/entities/tab.dart';
+import 'package:sensors_monitoring/src/domain/repositories/tab_repository.dart';
 
-class AddTabUsecase extends UseCase<Tab, AddTabUsecaseParams> {
+@Injectable()
+class AddTabUsecase extends UseCase<Unit, AddTabUsecaseParams> {
+  final TabRepository tabRepository;
+
+  AddTabUsecase({required this.tabRepository});
+
   @override
-  FutureOr<Either<Failure, Tab>> call(AddTabUsecaseParams param) {
-    // TODO: implement call
-    throw UnimplementedError();
+  FutureOr<Either<Failure, Unit>> call(AddTabUsecaseParams param) async {
+    return await tabRepository.addTab(param: param);
   }
 }
 
 class AddTabUsecaseParams {
-  final String configId;
-  final String title;
+  final Config config;
+  final Tab tab;
 
   AddTabUsecaseParams({
-    required this.configId,
-    required this.title,
+    required this.config,
+    required this.tab,
   });
 }
--- lib/src/domain/usecases/tab/edit_tab_usecase.dart
+++ lib/src/domain/usecases/tab/edit_tab_usecase.dart
@@ -1,24 +1,31 @@
 import 'dart:async';
 
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/usecase/usecase.dart';
+import 'package:sensors_monitoring/src/domain/entities/config.dart';
 import 'package:sensors_monitoring/src/domain/entities/tab.dart';
+import 'package:sensors_monitoring/src/domain/repositories/tab_repository.dart';
 
-class EditTabUsecase extends UseCase<Tab, EditTabUsecaseParams> {
+@Injectable()
+class EditTabUsecase extends UseCase<Unit, EditTabUsecaseParams> {
+  final TabRepository tabRepository;
+
+  EditTabUsecase({required this.tabRepository});
+
   @override
-  FutureOr<Either<Failure, Tab>> call(EditTabUsecaseParams param) {
-    // TODO: implement call
-    throw UnimplementedError();
+  FutureOr<Either<Failure, Unit>> call(EditTabUsecaseParams param) async {
+    return await tabRepository.editTab(param: param);
   }
 }
 
 class EditTabUsecaseParams {
-  final String id;
-  final String title;
+  final Config config;
+  final Tab tab;
 
   EditTabUsecaseParams({
-    required this.id,
-    required this.title,
+    required this.config,
+    required this.tab,
   });
 }
