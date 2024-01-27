8204e6ca375ed8c90b58c9b9cbb59b910c224f1b
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-01-27 15:29:52 +0000

    feat: add test alert repository implementation

--- lib/src/domain/repositories/alert_repository.dart
+++ lib/src/domain/repositories/alert_repository.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
+import 'package:sensors_monitoring/core/enum/alert_type.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/src/domain/entities/alert_data.dart';
 
@@ -15,13 +18,32 @@ abstract class AlertRepository {
 class UnimplementedNotificationRepository implements AlertRepository {
   @override
   Future<Either<Failure, Iterable<AlertData>>> get({required int count}) async {
-    // Test Right Example
+    final Random rndModule = Random.secure();
+    final Iterator<AlertType> alertTypeIter = AlertType.values.iterator;
+
+    return Right(
+      List<String>.generate(4, (index) => (rndModule.nextInt(4096) << 2).toString()).map((e) {
+        final alertData = AlertData(
+          id: e,
+          title: e,
+          message: 'TEST_MESSAGE',
+          description: 'TEST_DESCRIPTION',
+          type: alertTypeIter.current,
+          sensorRuleList: [],
+        );
+
+        alertTypeIter.moveNext();
+        return alertData;
+      }),
+    );
+
     // return Right([
-    //   AlertData(type: AlertType.error, title: 'Test Error', description: 'Test Error Description', datetime: DateTime.now()),
-    //   AlertData(type: AlertType.info, title: 'Test Info', description: 'Test Info Description', datetime: DateTime.now()),
-    //   AlertData(type: AlertType.warning, title: 'Test Warning', description: 'Test Warning Description', datetime: DateTime.now()),
+    //   AlertData(id: (rndModule.nextInt(4096) << 2).toString(), title: title, message: message, description: description, type: type, sensorRuleList: sensorRuleList)
+    //   // AlertData(type: AlertType.error, title: 'Test Error', description: 'Test Error Description', datetime: DateTime.now()),
+    //   // AlertData(type: AlertType.info, title: 'Test Info', description: 'Test Info Description', datetime: DateTime.now()),
+    //   // AlertData(type: AlertType.warning, title: 'Test Warning', description: 'Test Warning Description', datetime: DateTime.now()),
     // ]);
 
-    return const Left(Failure(message: 'Unimplemented [NotificationRepository] logic'));
+    // return const Left(Failure(message: 'Unimplemented [NotificationRepository] logic'));
   }
 }
