d881d39be87216102f1cddc22a1bd08c5eb91e52
diffygg <np.01@inbox.ru>
2024-01-27 18:51:42 +0300

    feat: add logic to methods

--- lib/src/data/repositories/tab_repository_impl.dart
+++ lib/src/data/repositories/tab_repository_impl.dart
@@ -3,19 +3,40 @@ import 'package:injectable/injectable.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 import 'package:sensors_monitoring/core/services/services.dart';
 import 'package:sensors_monitoring/src/data/datasources/common_datasource.dart';
+import 'package:sensors_monitoring/src/data/models/tabs_model.dart';
+import 'package:sensors_monitoring/src/domain/entities/config.dart';
+import 'package:sensors_monitoring/src/domain/entities/tab.dart';
 import 'package:sensors_monitoring/src/domain/repositories/tab_repository.dart';
 import 'package:sensors_monitoring/src/domain/usecases/tab/add_tab_usecase.dart';
 import 'package:sensors_monitoring/src/domain/usecases/tab/edit_tab_usecase.dart';
 
 @Injectable(as: TabRepository)
 class TabRepositoryImpl implements TabRepository {
-  final CommonDatasource commonDatasource = services<CommonDatasource>();
+  final CommonDatasource datasource;
+
+  TabRepositoryImpl({required this.datasource});
+
   @override
   Future<Either<Failure, Unit>> addTab({required AddTabUsecaseParams param}) async {
     try {
-      await commonDatasource.insertTabs(
-        configId: param.configId,
-        title: param.title,
+      final TabsModel tabsModel = await datasource.insertTabs(
+        configId: param.config.id,
+        title: param.tab.title,
+      );
+
+      for (final sensorInfo in param.tab.sensorInfoList) {
+        await datasource.insertTabSensors(
+          sensorId: sensorInfo.id,
+          tabId: tabsModel.id,
+        );
+      }
+
+      param.config.tabList.add(
+        Tab(
+          id: tabsModel.id,
+          sensorInfoList: param.tab.sensorInfoList,
+          title: tabsModel.title,
+        ),
       );
 
       return const Right(unit);
@@ -27,7 +48,7 @@ class TabRepositoryImpl implements TabRepository {
   @override
   Future<Either<Failure, Unit>> deleteTabById({required String id}) async {
     try {
-      await commonDatasource.deleteTabs(id: id);
+      await datasource.deleteTabsById(id);
 
       return const Right(unit);
     } catch (_) {
@@ -38,9 +59,30 @@ class TabRepositoryImpl implements TabRepository {
   @override
   Future<Either<Failure, Unit>> editTab({required EditTabUsecaseParams param}) async {
     try {
-      await commonDatasource.updateTabs(
-        id: param.id,
-        title: param.title,
+      // await datasource.updateTabs(
+      //   id: param.id,
+      //   title: param.title,
+      // );
+      await datasource.deleteTabsById(param.tab.id);
+
+      final TabsModel tabsModel = await datasource.insertTabs(
+        configId: param.config.id,
+        title: param.tab.title,
+      );
+
+      for (final sensorInfo in param.tab.sensorInfoList) {
+        await datasource.insertTabSensors(
+          sensorId: sensorInfo.id,
+          tabId: tabsModel.id,
+        );
+      }
+
+      param.config.tabList.add(
+        Tab(
+          sensorInfoList: param.tab.sensorInfoList,
+          id: tabsModel.id,
+          title: tabsModel.title,
+        ),
       );
 
       return const Right(unit);
@@ -52,7 +94,7 @@ class TabRepositoryImpl implements TabRepository {
   @override
   Future<Either<Failure, Unit>> getAllTabsByConfigId({required String configId}) async {
     try {
-      await commonDatasource.selectAllTabsByConfigId(configId);
+      await datasource.selectAllTabsByConfigId(configId);
 
       return const Right(unit);
     } catch (_) {
@@ -63,7 +105,7 @@ class TabRepositoryImpl implements TabRepository {
   @override
   Future<Either<Failure, Unit>> getTabById({required String id}) async {
     try {
-      await commonDatasource.selectOneTabsById(id: id);
+      await datasource.selectOneTabsById(id: id);
 
       return const Right(unit);
     } catch (_) {
