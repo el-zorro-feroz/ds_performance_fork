f0508c063f7daf922603db41d77ebfe74130ff7f
diffygg <np.01@inbox.ru>
2024-01-28 19:54:43 +0300

    feat: build_runner

--- lib/core/services/services.config.dart
+++ lib/core/services/services.config.dart
@@ -12,35 +12,45 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:sensors_monitoring/core/services/services.dart' as _i6;
 import 'package:sensors_monitoring/src/data/datasources/common_datasource.dart'
-    as _i11;
+    as _i9;
 import 'package:sensors_monitoring/src/data/repositories/config_repository_impl.dart'
-    as _i13;
+    as _i11;
 import 'package:sensors_monitoring/src/data/repositories/sensor_repository_impl.dart'
     as _i8;
 import 'package:sensors_monitoring/src/data/repositories/tab_repository_impl.dart'
-    as _i10;
+    as _i16;
 import 'package:sensors_monitoring/src/domain/repositories/alert_repository.dart'
     as _i3;
 import 'package:sensors_monitoring/src/domain/repositories/config_repository.dart'
-    as _i12;
+    as _i10;
 import 'package:sensors_monitoring/src/domain/repositories/sensor_repository.dart'
     as _i7;
 import 'package:sensors_monitoring/src/domain/repositories/tab_repository.dart'
-    as _i9;
+    as _i15;
 import 'package:sensors_monitoring/src/domain/usecases/alert/get_alerts_usecase.dart'
     as _i4;
 import 'package:sensors_monitoring/src/domain/usecases/configs/add_config_usecase.dart'
     as _i17;
 import 'package:sensors_monitoring/src/domain/usecases/configs/delete_config_by_id_usecase.dart'
-    as _i14;
+    as _i12;
 import 'package:sensors_monitoring/src/domain/usecases/configs/edit_config_usecase.dart'
-    as _i15;
+    as _i13;
 import 'package:sensors_monitoring/src/domain/usecases/configs/get_all_configs_usecase.dart'
-    as _i16;
-import 'package:sensors_monitoring/src/presentation/controllers/config_controller.dart'
+    as _i14;
+import 'package:sensors_monitoring/src/domain/usecases/tab/add_tab_usecase.dart'
     as _i18;
+import 'package:sensors_monitoring/src/domain/usecases/tab/delete_tab_by_id_usecase.dart'
+    as _i21;
+import 'package:sensors_monitoring/src/domain/usecases/tab/edit_tab_usecase.dart'
+    as _i22;
+import 'package:sensors_monitoring/src/presentation/controllers/config_controller.dart'
+    as _i19;
+import 'package:sensors_monitoring/src/presentation/controllers/config_settings_controller.dart'
+    as _i20;
 import 'package:sensors_monitoring/src/presentation/controllers/notification_controller.dart'
     as _i5;
+import 'package:sensors_monitoring/src/presentation/controllers/tab_controller.dart'
+    as _i23;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -61,21 +71,36 @@ extension GetItInjectableX on _i1.GetIt {
         getNotificationsUseCase: gh<_i4.GetAlertsUseCase>()));
     gh.singleton<_i6.PostgresModule>(_i6.PostgresModule());
     gh.factory<_i7.SensorRepository>(() => _i8.SensorRepositoryImpl());
-    gh.factory<_i9.TabRepository>(() => _i10.TabRepositoryImpl());
-    gh.factory<_i11.CommonDatasource>(
-        () => _i11.CommonDatasource(postgresModule: gh<_i6.PostgresModule>()));
-    gh.factory<_i12.ConfigRepository>(() =>
-        _i13.ConfigRepositoryImpl(datasource: gh<_i11.CommonDatasource>()));
-    gh.factory<_i14.DeleteConfigByIdUsecase>(() => _i14.DeleteConfigByIdUsecase(
-        configRepository: gh<_i12.ConfigRepository>()));
-    gh.factory<_i15.EditConfigUsecase>(
-        () => _i15.EditConfigUsecase(repository: gh<_i12.ConfigRepository>()));
-    gh.factory<_i16.GetAllConfigsUseCase>(() => _i16.GetAllConfigsUseCase(
-        configRepository: gh<_i12.ConfigRepository>()));
+    gh.factory<_i9.CommonDatasource>(
+        () => _i9.CommonDatasource(postgresModule: gh<_i6.PostgresModule>()));
+    gh.factory<_i10.ConfigRepository>(() =>
+        _i11.ConfigRepositoryImpl(datasource: gh<_i9.CommonDatasource>()));
+    gh.factory<_i12.DeleteConfigByIdUsecase>(() => _i12.DeleteConfigByIdUsecase(
+        configRepository: gh<_i10.ConfigRepository>()));
+    gh.factory<_i13.EditConfigUsecase>(
+        () => _i13.EditConfigUsecase(repository: gh<_i10.ConfigRepository>()));
+    gh.factory<_i14.GetAllConfigsUseCase>(() => _i14.GetAllConfigsUseCase(
+        configRepository: gh<_i10.ConfigRepository>()));
+    gh.factory<_i15.TabRepository>(
+        () => _i16.TabRepositoryImpl(datasource: gh<_i9.CommonDatasource>()));
     gh.factory<_i17.AddConfigUsecase>(() =>
-        _i17.AddConfigUsecase(configRepository: gh<_i12.ConfigRepository>()));
-    gh.singleton<_i18.ConfigController>(_i18.ConfigController(
-        getAllConfigsUseCase: gh<_i16.GetAllConfigsUseCase>()));
+        _i17.AddConfigUsecase(configRepository: gh<_i10.ConfigRepository>()));
+    gh.factory<_i18.AddTabUsecase>(
+        () => _i18.AddTabUsecase(tabRepository: gh<_i15.TabRepository>()));
+    gh.singleton<_i19.ConfigController>(_i19.ConfigController(
+        getAllConfigsUseCase: gh<_i14.GetAllConfigsUseCase>()));
+    gh.factory<_i20.ConfigSettingsController>(
+        () => _i20.ConfigSettingsController(
+              addConfigUsecase: gh<_i17.AddConfigUsecase>(),
+              editConfigUsecase: gh<_i13.EditConfigUsecase>(),
+              deleteConfigByIdUsecase: gh<_i12.DeleteConfigByIdUsecase>(),
+            ));
+    gh.factory<_i21.DeleteTabByIdUsecase>(() =>
+        _i21.DeleteTabByIdUsecase(tabRepository: gh<_i15.TabRepository>()));
+    gh.factory<_i22.EditTabUsecase>(
+        () => _i22.EditTabUsecase(tabRepository: gh<_i15.TabRepository>()));
+    gh.factory<_i23.TabController>(
+        () => _i23.TabController(addTabUsecase: gh<_i18.AddTabUsecase>()));
     return this;
   }
 }
