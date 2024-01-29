78910778c6f66d35d4fa74368f647716475fe0ee
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-01-29 21:35:29 +0000

    docs: provide api docs for usecase class

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'package:dartz/dartz.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
 
+/// A base class that allows us to create convenient interfaces
+/// whose method will return an optional response.
 abstract class UseCase<T, P> {
-  FutureOr<Either<Failure, T>> call(P param);
+  /// Method that will return an optional response as an [Failure]
+  /// or [T] accepting [P] as a request data
+  FutureOr<Either<Failure, T>> call(P data);
 }
