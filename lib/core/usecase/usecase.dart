69fa9a72e6c802d06663bd660c90a8c5f23aeacb
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-01-29 22:53:42 +0000

    feat(core): add some new types to work with either values

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,12 +1,10 @@
-import 'dart:async';
-
-import 'package:dartz/dartz.dart';
 import 'package:sensors_monitoring/core/failure/failure.dart';
+import 'package:sensors_monitoring/core/types/fail_or.dart';
 
 /// A base class that allows us to create convenient interfaces
 /// whose method will return an optional response.
 abstract class UseCase<T, P> {
   /// Method that will return an optional response as an [Failure]
   /// or [T] accepting [P] as a request data
-  FutureOr<Either<Failure, T>> call(P data);
+  FFailOr<T> call(P data);
 }
