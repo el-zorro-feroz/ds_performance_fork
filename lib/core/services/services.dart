bd7ad46a35dd67563e02d9c689fb652f9d07d170
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-01-29 21:20:42 +0000

    feat: remove postrgres module from core services

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -2,26 +2,14 @@ import 'dart:async';
 
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:postgres/postgres.dart';
 
-import 'services.config.dart';
+import 'package:sensors_monitoring/core/services/services.config.dart';
 
+/// Global Service locator implementation
 final GetIt services = GetIt.I;
 
+/// Global [services] initialization method
 @InjectableInit()
 Future<void> servicesInit() async {
   services.init();
-  await services<PostgresModule>().initPostgres();
-}
-
-@Singleton()
-class PostgresModule {
-  static late PostgreSQLConnection postgreSQLConnection;
-
-  FutureOr<void> initPostgres() async {
-    postgreSQLConnection = PostgreSQLConnection("127.0.0.1", 5432, "postgres", username: "postgres", password: "1");
-    if (postgreSQLConnection.isClosed) {
-      await postgreSQLConnection.open();
-    }
-  }
 }
