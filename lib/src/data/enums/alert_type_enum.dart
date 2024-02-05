a0356792fb076f4d92d664d4efed6e4b8efa2ca6
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-02-05 17:20:20 +0300

    feat(freezed): convert alert type to freezed object

--- lib/src/data/enums/alert_type_enum.dart
+++ lib/src/data/enums/alert_type_enum.dart
@@ -1,18 +1,34 @@
-/// Available alert types for sensor alert system.
-enum AlertType {
-  /// Used for notification purposes,
-  /// e.g. low temperature increase.
-  info,
+// DEPRECATED. Converted to freezed object.
+// /// Available alert types for sensor alert system.
+// enum AlertType {
+//   /// Used for notification purposes,
+//   /// e.g. low temperature increase.
+//   info,
 
-  /// Used for purposes where attention needs to be focused,
-  /// e.g. temperature jump higher than normal.
-  warning,
+//   /// Used for purposes where attention needs to be focused,
+//   /// e.g. temperature jump higher than normal.
+//   warning,
 
-  /// Used for errors in sensors,
-  /// e.g. unexpected sensor value jumps.
-  error,
+//   /// Used for errors in sensors,
+//   /// e.g. unexpected sensor value jumps.
+//   error,
 
-  /// Used for purposes when something fails, etc.,
-  /// e.g. sensor connections breakdown.
-  fatal,
+//   /// Used for purposes when something fails, etc.,
+//   /// e.g. sensor connections breakdown.
+//   fatal,
+// }
+
+/// Available alert types for alert system.
+class AlertType with _$AlertType {
+  /// Used when ???
+  const factory AlertType.info() = _InfoAlertType;
+
+  /// 
+  const factory AlertType.warning() = _InfoAlertType;
+
+  /// 
+  const factory AlertType.error() = _InfoAlertType;
+
+  /// 
+  const factory AlertType.fatal() = _FatalAlertType;
 }
